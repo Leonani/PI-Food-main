6cbe1cb3f49b28785db9a8529bd4c66bda71db81
LEONEL ESCUDERO <71057664+Leonani@users.noreply.github.com>
2021-12-01 18:14:54 -0300

    toma dispatch
    jajajajaja llavesitas llavesitas

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
-import {store} from './store/index'
+import store from './store/index'
 
 //se envuelve el render en un 'provider' para que funcione redux
 ReactDOM.render(
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -15,29 +15,29 @@
 
 //-------------------------------------------------------------------------------------------
 
-// import { createStore, applyMiddleware, compose } from "redux";
-// import rootReducer from "../reducer/index";
-// import thunk from "redux-thunk";
-
-// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
-
-// export default store;
-//---------------------------------------------------------------------------------------------
-
 import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "../reducer";
+import rootReducer from "../reducer/index";
 import thunk from "redux-thunk";
 
-export const store = createStore(
-    rootReducer,
-    compose(
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(
-            applyMiddleware(thunk),
-        )
-    )
-)
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+
+export default store;
+//---------------------------------------------------------------------------------------------
+
+// import { createStore, applyMiddleware, compose } from "redux";
+// import rootReducer from "../reducer";
+// import thunk from "redux-thunk";
+
+// export const store = createStore(
+//     rootReducer,
+//     compose(
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(
+//             applyMiddleware(thunk),
+//         )
+//     )
+// )
 //-------------------------------------------------------------------------------------------------
 // import { createStore} from 'redux';
 
